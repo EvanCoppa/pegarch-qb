795d2e1db316c21ab35224e6fdd93f9c6edbb67b
Evan Coppa <42395557+EvanCoppa@users.noreply.github.com>
2025-05-30 22:20:05 -0400

    refactor: clean up project rendering logic and improve timesheet display

--- frontend/src/routes/invoices/[projectId]/+page.svelte
+++ frontend/src/routes/invoices/[projectId]/+page.svelte
@@ -2,7 +2,6 @@
   import { page } from "$app/state";
   import { onMount } from "svelte";
   let projects = [];
- 
 
   onMount(async () => {
     const results = page.params.projectId;
@@ -22,37 +21,35 @@
       // console.log(project);
       const res = await fetch(`http://localhost:3000/api/projects/${project.project_id}/timecard-dates`);
       if (res.ok) {
-         const timesheet = await res.json(); // Assuming this is an array of timecards
-          project.timesheets = timesheet; // Add as a new property to the project object
-          projects = projects;
+        const timesheet = await res.json(); // Assuming this is an array of timecards
+        project.timesheets = timesheet; // Add as a new property to the project object
+        projects = projects;
       }
     }
   });
 </script>
- {#if projects.length != 0}
-   
+
+{#if projects.length != 0}
   <div class="flex flex-col">
-
-{#each projects as project}
-  <div class="flex flex-col w-full overflow-x-hidden bg-white p-4">
-    <div class="flex flex-row justify-between m-4">
-      <h1 class="text-3xl font-semibold my-auto">{project.project_id} {project.name}</h1>
-    </div>
-     {console.log("Project", project)}
-     {#if project.timesheets}
-      {#each project.timesheets as timesheet}
-      <h2>
-        {timesheet.start_date} - {timesheet.end_date} - Total Hours: {timesheet.total_hours}
-      </h2>
-     {/each}
-      
-     
-     {/if}
-
-     
+    {#each projects as project}
+      {#if project.timesheets && project.timesheets.length > 0}
+        <div class="flex flex-col w-full overflow-x-hidden bg-white p-4">
+          <div class="flex flex-row justify-between m-4">
+            <h1 class="text-3xl font-semibold my-auto">{project.project_id} {project.name}</h1>
+          </div>
+          {#each project.timesheets as timesheet}
+            <h2>
+              {timesheet.start_date} - {timesheet.end_date} - Total Hours: {timesheet.total_hours}
+            </h2>
+          {/each}
+        </div>
+      {:else}
+        <div class="flex flex-col w-full h-full overflow-x-hidden bg-white p-4">
+          <div class="flex flex-row justify-between m-4">
+            <h1 class="text-3xl font-semibold my-auto">No Projects Found</h1>
+          </div>
+        </div>
+      {/if}
+    {/each}
   </div>
-{/each}
-
-  </div>
-
- {/if}
+{/if}
